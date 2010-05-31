22ad23639ed5a2be345360008121f7ff6f74248a
tarasm <tarasm@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-05-31 16:00:38 +0000

    Added the tag and refactored the code
    git-svn-id: https://plugins.svn.wordpress.org/site-plugin-core/tags/0.2.4@247573 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ templates/after_upgrade.php
@@ -0,0 +1,12 @@
+<?php 
+/*
+ * This file is executed after performing the upgrade. This can be used to verify that
+ * the changes peformed as expected.
+ * 
+ * If this these tests are going to be used continuously to test the site,
+ * then you should put them in pregression_tests.php instead.
+ * 
+ */
+defined('SITEPLUGIN') or wp_die('This script can only be executed from inside wp-admin');
+
+?>
